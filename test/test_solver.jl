24e901f3adcfe81a6e778bb90e74dc1d95c2f858
Dan F-M <danfm@nyu.edu>
2017-03-24 15:43:16 -0700

    adding unit test

---
+++ test/test_solver.jl
@@ -0,0 +1,29 @@
+using Base.Test
+import celerite
+
+function test_solver()
+    srand(42)
+    N = 100
+    x = sort(10*rand(N))
+    y = sin(x)
+    yerr = 0.01 + 0.01*rand(N)
+
+    kernel = celerite.RealTerm(0.5, 1.0) + celerite.SHOTerm(0.1, 2.0, -0.5)
+    gp = celerite.Celerite(kernel)
+
+    K = celerite.get_matrix(gp, x)
+    for n in 1:N
+        K[n, n] = K[n, n] + yerr[n]^2
+    end
+
+    # Compute using celerite
+    celerite.compute!(gp, x, yerr)
+    ll = celerite.log_likelihood(gp, y)
+
+    # Compute directly
+    ll0 = -0.5*(sum(y .* (K \ y)) + logdet(K) + N*log(2.0*pi))
+
+    @test isapprox(ll, ll0)
+end
+
+test_solver()
