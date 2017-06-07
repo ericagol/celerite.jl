2a517aaa747272ccfe9b12a319303edca27cfab0
Eric Agol <agol@uw.edu>
2017-06-07 16:26:14 -0700

    Cleaning up test code

--- test/test_cholesky.jl
+++ test/test_cholesky.jl
@@ -1,5 +1,5 @@
-# Translating DFM's python version:
-@testset "Cholesky LDLT" for (J0, J) in ((1, 1), (0, 16))
+# Test suite for gp.jl
+@testset "Cholesky LDLT" for (J0, J, N_test) in ((1, 1, [64,1024]), (0, 16, [32,512]), (9,9,[14893348]))
     # J0 = Number of real, exponential celerite kernel terms
     # J  = Total number of terms
 
@@ -23,12 +23,7 @@
         end
     end
 
-
     # Run a timing test:
-    #N_test = [64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288]
-    N_test = [512]
-    #N_test = [64,256,1024,4096,16384]
-    #N_test = [N]
     ntest = length(N_test)
     time_complex = zeros(Float64,ntest)
     itest = 1
@@ -59,23 +54,26 @@
         end
     end
     gp = celerite.Celerite(kernel)
-    # Cholesky method
-    # Compute Cholesky factorization:
+    # The following was used for profiling the code:
     if itest == 9
         logdet_test = celerite.compute_ldlt!(gp, t, yerr)
         @profile logdet_test = celerite.compute_ldlt!(gp, t, yerr)
         Profile.print(format=:flat)
         #  else
     end
+    # Cholesky method
+    # Compute Cholesky factorization, return log of determinant:
     logdet_test = celerite.compute_ldlt!(gp, t, yerr)
     # Generate random noise, and a realization of the GP:
     noise = randn(N)
+    # This simulates a Gaussian process with "gp" kernel:
     y0 = celerite.simulate_gp_ldlt(gp,noise)
-    # Check that this inverse works:
+    # Check that the inverse works:
     noise_test = celerite.invert_lower_ldlt(gp,y0)./sqrt(gp.D)
 #    println("noise recovered? ",maximum(abs(noise-noise_test)))
     @test isapprox(noise,noise_test)
     time_zero = tic()
+    # Run a somme trials to get a timing estimate:
     for itrial = 1:ntrial
         logdet_test = celerite.compute_ldlt!(gp, t, yerr)
     end
@@ -85,7 +83,7 @@
     if N < 2000
         logdetK,K = celerite.full_solve(t,y0,aj,bj,cj,dj,yerr)
 # Check the log determinant:
-        println("Log det: ",logdetK,logdet_test)
+#        println("Log det: ",logdetK,logdet_test)
         @test isapprox(logdetK,logdet_test)
 # Check that simulation works:
         K_lower = chol(K)'  # chol(K) returns upper triangular cholesky matrix, so we need to transpose.
@@ -121,7 +119,7 @@
         @test isapprox(L, K_lower)
 #        println("Cholesky error: ",maximum(abs(L - K_lower)))
         # Check that reconstructed matrix agrees with full covariance matrix:
-        @test isapprox(abs(*(L, L'), K))
+        @test isapprox(*(L, L'), K)
 #        println("Cholesky error: ",maximum(abs(*(L, L') - K)))
 #        println("y0: ",y0)
 #        println("y0_full: ",y0_full)
@@ -135,40 +133,22 @@
 #        println("K \\ y0: ",maximum(abs(z_full-z)))
         @test isapprox(z_full,z)
 # Check that the "chi-square" gives the correct value:
-        println("N: ",N," dot(y0,z): ",dot(y0,z)," dot(noise,noise): ",dot(noise,noise))
-#        @test isapprox(dot(y0,z),dot(noise,noise))
+#        println("N: ",N," dot(y0,z): ",dot(y0,z)," dot(noise,noise): ",dot(noise,noise))
+        @test isapprox(dot(y0,z),dot(noise,noise))
 # Check that the log likelihood is computed correctly:
         nll = celerite.log_likelihood_ldlt(gp, y0)
-        nll_full = -.5*(logdetK+N*log(2pi)+dot(z_full,z_full))
-        println("Log likelihoods: ",nll," ",nll_full)
+        nll_full = -.5*(logdetK+N*log(2pi)+dot(y0,z))
+#        println("Log likelihoods: ",nll," ",nll_full)
         @test isapprox(nll,nll_full)
+        println(N_test[itest]," ",time_complex[itest])
+        time_prior = time_complex[itest]
+        M = N*4
+        tpred = sort!(rand(M)) .* 200
+        ypred = celerite.predict_ldlt!(gp, t, y0, tpred)
+        ypred_full = celerite.predict_full_ldlt(gp, y0, tpred; return_cov = false)
+    #end
+        @test isapprox(ypred,ypred_full)
     end
-    println(N_test[itest]," ",time_complex[itest])
-    time_prior = time_complex[itest]
-    M = N*4
-    tpred = sort!(rand(M)) .* 200
-    ypred = celerite.predict_ldlt!(gp, t, y0, tpred)
-    ypred_full = celerite.predict_full_ldlt(gp, y0, tpred; return_cov = false)
-    itest +=1
-    #end
-    @test isapprox(ypred,ypred_full)
-
-    #loglog(N_test,time_complex)
-    data = readdlm("c_speed.txt",',')
-    N_c = vec(data[:,4])
-    t_c = vec(data[:,5])
-    #loglog(N_test,(N_test./256).*2e-3)
-    #loglog(N_c,t_c)
-    #println(time_complex./((N_test./256).*2e-3))
-    println(time_complex./t_c)
-
-    ## Convert from X' back to X:
-    ## (Not sure if I need to do this)
-    #for n=1:N
-    #  for j=1:J
-    #    X[n,2*j-1] *= exp(cj[j]*t[n])
-    #    X[n,2*j  ] *= exp(cj[j]*t[n])
-    #  end
-    #end
-
+    println("J0,J: ",J0," ",J," N: ",N)
 end
+return
